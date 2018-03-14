55d2ff994f1e595ceee27619eb2e0f513af908aa
GaoWei <qazmichaelgw@gmail.com>
2018-03-14 21:36:41 +0800

    test

--- js/projects.js
+++ js/projects.js
@@ -4,6 +4,17 @@ oss_projects['league'] = {
   featured: true,
   position: 1,
   background: 'uefa2_bg.png'
-}
-
+};
+oss_projects['dockersh'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2014/08/hack209-dockersh.html',
+  featured: true,
+  position: 2,
+  background: 'dockersh_bg.png'
+};
+oss_projects['osxcollector'] = {
+  blog_post: 'https://engineeringblog.yelp.com/2015/01/osxcollector-forensic-collection-and-automated-analysis-for-os-x.html',
+  featured: true,
+  position: 3,
+  background: 'osxcollector_bg.png'
+};
 // vim: sw=2 sts=2 expandtab
