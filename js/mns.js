f769fd05952dfbe99e29474947061e5c73d3e3ec
GaoWei <qazmichaelgw@gmail.com>
2018-03-15 10:55:43 +0800

    debug

--- js/mns.js
+++ js/mns.js
@@ -41,7 +41,7 @@ MasterNode.prototype.getContainer = function(index){
 MasterNode.prototype.logoImage = function(){
     if (this.logo()){
         return [
-            '<div class="island-item logo-image">',
+            '<div class="island-item featured-image">',
             '<img src="/img/', this.logo() ,'">',
             '</div>'
         ].join('');
@@ -55,8 +55,8 @@ MasterNode.prototype.masternodeContent = function(){
         '<a href="', this.website(), '" target="_blank">', this.name, ' (',this.alias,')</a>',
       '</h3>',
       '<div class="mn-info">',
-        '<span><i class="coin coin-usd-price"></i> USD:', this.usd_price, '</span> ',
-        '<span><i class="coin coin-btc-price"></i> BTC:', this.btc_price, '</span>',
+        '<span><i class="octicon coin-usd-price"></i> USD:', this.usd_price, '</span> ',
+        '<span><i class="octicon coin-btc-price"></i> BTC:', this.btc_price, '</span>',
       '</div>',
       '<p>', this.description, '</p>',
     '</div>'
@@ -64,11 +64,11 @@ MasterNode.prototype.masternodeContent = function(){
 }
 
 $(document).ready(function(){
-    $('.masternodes .coins').empty();
+    $('.project .coins').empty();
     var i = 0;
     for (coin in coins){
         var masternode = new MasterNode(coin, coins[coin]);
-        $('.masternodes .coins').append(masternode.getContainer(i+1));
+        $('.project .coins').append(masternode.getContainer(i+1));
     }
     $('.masternodes-count').html(coins.length);
 });
