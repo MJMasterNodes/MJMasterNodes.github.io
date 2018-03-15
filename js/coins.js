373cc17d08b249ba95783d48c2c9072c8def640c
GaoWei <qazmichaelgw@gmail.com>
2018-03-15 21:09:53 +0800

    add friendship coin

--- js/coins.js
+++ js/coins.js
@@ -7,3 +7,11 @@ coins['league'] = {
     usd_price: '0.5',
     MNs: [['http://explorer.league.express:3003/address/mxkqvoGXQfXCyfPFtZ5UmeyM2Q5BN2cjX6','https://docs.google.com/spreadsheets/d/1sgP3jBM8O0PJpi3aYBR0F4LgIYNLlJ9gGSIMX3zWswo/edit?usp=sharing'],]
 };
+coins['friendship'] = {
+    alias : 'FSC',
+    website : 'https://www.friendshipcoin.com',
+    logo : 'friendship.png',
+    description : 'Everyone can see it, but only you can feel the warmth. We want to spread the love one small blockchain transaction at a time. Lets put a new currency to work for the world.',
+    usd_price: '0.018',
+    MNs: [['https://explore.friendshipcoin.com/address/fTD7LMR4cmuV4t9pHPNBU5GRdsqwGJqjeA','https://docs.google.com/spreadsheets/d/17Wns2Ol_wZmQf7n522hx5X-LJ7map2eDdi9TGF0wtY4/edit?usp=sharing'],]
+};
