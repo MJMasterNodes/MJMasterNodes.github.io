39ff17820d2665c49b1c014400a5fd73d97ee75b
GaoWei <qazmichaelgw@gmail.com>
2018-04-11 11:08:20 +0800

    add LGA MN

--- js/coins.js
+++ js/coins.js
@@ -4,8 +4,8 @@ coins['league'] = {
     website : 'https://league.express',
     logo : 'uefa2_bg.png',
     description : 'League coin is made special for 2018 FIFA World Cup and will be integrated with a lot of betting platforms. As soon as 2018 FIFA World Cup in Russia starts you can use your coins for bets.',
-    usd_price: '0.5',
-    MNs: [['http://explorer.league.express:3003/address/mxkqvoGXQfXCyfPFtZ5UmeyM2Q5BN2cjX6','https://docs.google.com/spreadsheets/d/1sgP3jBM8O0PJpi3aYBR0F4LgIYNLlJ9gGSIMX3zWswo/edit?usp=sharing'],['http://explorer.league.express:3003/address/mvxDdreYozfEESoFXNTdoc1JNxgaAjXJoA','https://docs.google.com/spreadsheets/d/1orgjsPmAZqWYgeMFHlSm2LHLPp2AYDV54WYE9QfeOXw/edit?usp=sharing', 'http://explorer.league.express:3003/address/mxnyRUMYtaNg6hwf4tJDppCWBr1E5mkGBg']]
+    usd_price: '1.5',
+    MNs: [['http://explorer.league.express:3003/address/mxkqvoGXQfXCyfPFtZ5UmeyM2Q5BN2cjX6','https://docs.google.com/spreadsheets/d/1sgP3jBM8O0PJpi3aYBR0F4LgIYNLlJ9gGSIMX3zWswo/edit?usp=sharing'],['http://explorer.league.express:3003/address/mvxDdreYozfEESoFXNTdoc1JNxgaAjXJoA','https://docs.google.com/spreadsheets/d/1orgjsPmAZqWYgeMFHlSm2LHLPp2AYDV54WYE9QfeOXw/edit?usp=sharing'], ['http://explorer.league.express:3003/address/mxnyRUMYtaNg6hwf4tJDppCWBr1E5mkGBg', 'https://docs.google.com/spreadsheets/d/1orgjsPmAZqWYgeMFHlSm2LHLPp2AYDV54WYE9QfeOXw/edit?usp=sharing']]
 };
 coins['friendship'] = {
     alias : 'FSC',
