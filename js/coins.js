fcde4544c656f6bee9696a28043cf06d4ffdefc6
GaoWei <qazmichaelgw@gmail.com>
2018-04-27 12:26:55 +0800

    remove useless coin

--- js/coins.js
+++ js/coins.js
@@ -15,38 +15,6 @@ coins['friendship'] = {
     usd_price: '0.15',
     MNs: [['https://explore.friendshipcoin.com/address/fTD7LMR4cmuV4t9pHPNBU5GRdsqwGJqjeA','https://docs.google.com/spreadsheets/d/17Wns2Ol_wZmQf7n522hx5X-LJ7map2eDdi9TGF0wtY4/edit?usp=sharing'],]
 };
-coins['international'] = {
-    alias : 'IMC',
-    website : 'http://intermodalcoin.online',
-    logo : 'internationalcoin.png',
-    description : 'Intermodal Coin is developed out of need in the Intermodal industry with the purpose to solve traditional Intermodal problems.',
-    usd_price: '1.0',
-    MNs: [['http://explorer.intermodalcoin.online/address/SkGhNKZbvHDiThP9XQ5NSUMawyPBvVM6Ug','https://docs.google.com/spreadsheets/d/1z-q9lBiqplKCxvt2TqKN-Uw4Bj6cwCQ-SieLGtmMan4/edit?usp=sharing'],]
-};
-coins['Pyro'] = {
-    alias : 'PYRO',
-    website : 'https://pyro.solutions',
-    logo : 'Pyro.png',
-    description : "Pyro is new digital cryptocurrency based on improved Dash blockchain. Established on a Proof of Work and Masternode system, this means everyone  can participate in Pyro's growth, to make  Pyro more decentralized without a monopoly.",
-    usd_price: '1.0',
-    MNs: [['','https://docs.google.com/spreadsheets/d/1Srn-B5f3YPGGddqsLDhj8BEaLUCgXg1P8AgS9dB0FXc/edit?usp=sharing'],]
-};
-coins['banq'] = {
-    alias : 'BANQ',
-    website : 'http://banq.online',
-    logo : 'banq.png',
-    description : 'BANQ is innovative decentralized global payment system based on blockchain technology and secured by Masternode system.',
-    usd_price: '10.0',
-    MNs: [['http://128.199.42.147:3001/address/BPQ3PHPSkeDFePKa9QvWFkD8g6g7vszhyF','https://docs.google.com/spreadsheets/d/1WsZO7wMduvouZrLOLVJZaxdUIgvea-tau1FrdEYPOrg/edit?usp=sharing'],]
-};
-coins['YOBA'] = {
-    alias : 'YOBA',
-    website : 'https://yoba.world',
-    logo : 'YOBA.png',
-    description : 'Be fast as YOBA. Be popular as YOBA. Be in the know as YOBA. YOBA is the best in the internet and YOBA Coin will be the best too. YOBA is similar to BTC. It is round and yellow too.',
-    usd_price: '0.4',
-    MNs: [['','https://docs.google.com/spreadsheets/d/1xTs3LHFuqYoiRiVtyrA-2A6PzhRDonlz_tZ8TBUvV9I/edit?usp=sharing'],]
-};
 coins['APOLLON'] = {
     alias : 'XAP',
     website : 'http://apolloncoin.io',
